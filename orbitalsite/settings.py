5cf8bb2c8474efe6a5fe6f35a1253f61cd6a0522
jacques <jacquesliaozeli@hotmail.com>
2022-07-28 01:25:40 +0800

    set debug to false for heroku deployment

--- orbitalsite/settings.py
+++ orbitalsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%k2(@-nx#w=r@hewget5lc1x=t1dj7pntu2!z^)bkvfw3hagoj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
